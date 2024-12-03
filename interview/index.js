e55c4dbffeacf419d184718df31cadc051300418
Afaiz8720 <96206044+Afaiz8720@users.noreply.github.com>
2024-12-03 20:00:56 +0530

    updated ui

---
+++ interview/index.js
@@ -0,0 +1,13 @@
+const people = [
+    // {
+    //     id: 1,
+    //     name: "Faiz Ahmad",
+    // },
+    // {
+    //     id: 2,
+    //     name: "Amir Ahmad",
+    // }
+]
+
+
+console.log(data)
